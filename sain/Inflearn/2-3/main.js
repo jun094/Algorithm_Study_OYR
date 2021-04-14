750387b40e16c3793b1587e0ba3ccbd0e51620db
Sain Choi <chyh1900@gmail.com>
2021-04-14 17:23:17 +0900

    Update main.js

--- sain/Inflearn/2-3/main.js
+++ sain/Inflearn/2-3/main.js
@@ -1,15 +1,31 @@
 console.log("main.js");
 
-function solution(numbers) {
+function solution(N, arrayA, arrayB) {
   let answer = "";
-
+  for (let i = 0; i < N; i++) {
+    if (arrayA[i] === arrayB[i]) {
+      answer += "D\n";
+    } else if (
+      (arrayA[i] === 1 && arrayB[i] === 3) ||
+      (arrayA[i] === 2 && arrayB[i] === 1) ||
+      (arrayA[i] === 3 && arrayB[i] === 2)
+    ) {
+      answer += "A\n";
+    } else {
+      answer += "B\n";
+    }
+  }
   return answer;
 }
 
-let numbers = prompt("배열 입력")
+let N = parseInt(prompt("N 입력"));
+let arrayA = prompt("A 배열 입력")
+  .split(",")
+  .map((d) => parseInt(d));
+let arrayB = prompt("B 배열 입력")
   .split(",")
   .map((d) => parseInt(d));
 
-console.log(numbers);
+console.log(N, arrayA, arrayB);
 
-console.log(solution(numbers));
+console.log(solution(N, arrayA, arrayB));
