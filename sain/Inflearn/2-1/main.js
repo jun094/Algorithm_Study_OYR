53cad60a9e41117cd2e57254f6049d07bd5b8238
Sain Choi <chyh1900@gmail.com>
2021-04-15 13:52:13 +0900

    inflearn 2-1 fix

--- sain/Inflearn/2-1/main.js
+++ sain/Inflearn/2-1/main.js
@@ -1,29 +1,16 @@
 console.log("main.js");
 
-function solution(N, array) {
-  let answer = [];
+function solution(array) {
+  let answer = "";
   let arrLen = array.length;
-  let findBefore = 0;
-  for (let i = 0; i < arrLen; i++) {
-    if (array[i] === N) {
-      if (i === 0) findBefore = array[i];
-      else findBefore = array[i - 1];
-      break;
-    }
-  }
 
   for (let i = 0; i < arrLen; i++) {
-    if (array[i] > findBefore) answer.push(array[i]);
+    if (i === 0 || array[i - 1] < array[i]) answer += array[i] + " ";
   }
 
   return answer;
 }
 
-let N = parseInt(prompt("자연수 N 입력"));
-let array = prompt("배열 입력")
-  .split(",")
-  .map((d) => parseInt(d));
+const array = [7, 3, 9, 5, 6, 12];
 
-console.log(N, array);
-
-console.log(solution(N, array));
+console.log(solution(array));
