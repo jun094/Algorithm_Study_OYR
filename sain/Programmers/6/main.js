b9857ff263637d9c2e6e89a5eb63e8ea792935d0
Sain Choi <chyh1900@gmail.com>
2021-04-17 16:11:00 +0900

    프로그래머스 구명보트 픽스 필요

---
+++ sain/Programmers/6/main.js
@@ -0,0 +1,15 @@
+console.log("main.js");
+
+function solution(number, k) {
+  let answer = 0;
+
+  return answer;
+}
+
+const input1 = { n: "1924", k: 2 };
+const input2 = { n: "1231234", k: 3 };
+const input3 = { n: "4177252841", k: 4 };
+
+const { n, k } = input1;
+
+console.log(solution(n, k));
