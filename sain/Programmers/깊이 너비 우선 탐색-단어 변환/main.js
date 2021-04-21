b52be459b6227c6301f41211681b7e0b2606b72a
Sain Choi <chyh1900@gmail.com>
2021-04-22 03:31:31 +0900

    으아아아앍

---
+++ sain/Programmers/깊이 너비 우선 탐색-단어 변환/main.js
@@ -0,0 +1,47 @@
+console.log("main.js");
+//https://webigotr.tistory.com/309
+
+function solution(begin, target, words) {
+  let answer = 0;
+  const arrLen = words.length;
+  const visited = {};
+  const q = [];
+
+  q.push(begin);
+  visited[begin] = 0;
+
+  while (q.length > 0) {
+    let currentWord = q.shift();
+    for (let i = 0; i < arrLen; i++) {
+      let nextWord = words[i];
+      const currents = currentWord.split("");
+      const nexts = nextWord.split("");
+      if (
+        visited[nextWord] === undefined &&
+        nexts.filter((d, i) => d !== currents[i]).length <= 1
+      ) {
+        q.push(nextWord);
+        visited[nextWord] = visited[currentWord] + 1;
+      }
+    }
+  }
+
+  answer = visited[target] ? visited[target] : 0;
+  return answer;
+}
+
+const input1 = {
+  begin: "hit",
+  target: "cog",
+  words: ["hot", "dot", "dog", "lot", "log", "cog"],
+};
+
+const input2 = {
+  begin: "hit",
+  target: "cog",
+  words: ["hot", "dot", "dog", "lot", "log"],
+};
+
+const { begin, target, words } = input1;
+
+console.log(solution(begin, target, words));
