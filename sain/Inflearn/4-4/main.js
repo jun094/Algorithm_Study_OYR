93a4cdf1a6c892ba15a5a404f895e9cfa76a3cfe
Sain Choi <chyh1900@gmail.com>
2021-04-28 17:33:42 +0900

    인프런 / 완전탐색 / K번째 큰 수

---
+++ sain/Inflearn/4-4/main.js
@@ -0,0 +1,19 @@
+console.log("main.js");
+
+function solution(input) {
+  let answer = null;
+  const [stdNums, budget] = input.splice(0, 1);
+
+  return answer;
+}
+
+const input = [
+  [5, 28],
+  [6, 6],
+  [2, 2],
+  [4, 3],
+  [4, 5],
+  [10, 3],
+];
+
+console.log(solution(input));
