eb85131680e607821e8975d846cce5fdde9617ce
Sain Choi <chyh1900@gmail.com>
2021-04-29 14:17:22 +0900

    인프런 / 완전탐색 / 졸업선물

--- sain/Inflearn/4-4/main.js
+++ sain/Inflearn/4-4/main.js
@@ -1,8 +1,25 @@
 console.log("main.js");
 
 function solution(input) {
-  let answer = null;
-  const [stdNums, budget] = input.splice(0, 1);
+  let answer = 0;
+  const [stdNums, budget] = input.splice(0, 1)[0];
+  input.sort((a, b) => a[0] + a[1] - (b[0] + b[1]));
+  const arrLen = input.length;
+
+  for (let i = 0; i < arrLen; i++) {
+    let sum = 0;
+    let ableStudents = 1;
+    // 각 상품을 50% 할인받았을 때 우선 합산
+    sum += input[i][0] / 2 + input[i][1];
+    for (let j = 0; j < arrLen; j++) {
+      const amount = input[j][0] + input[j][1];
+      if (j === i) continue;
+      else if (sum + amount > budget) break;
+      sum += amount;
+      ableStudents++;
+    }
+    if (answer < ableStudents) answer = ableStudents;
+  }
 
   return answer;
 }
