d8e3a354948587b8fdfcf5c9e9138a91d81df400
Sain Choi <chyh1900@gmail.com>
2021-05-17 16:15:47 +0900

    프로그래머스 블록 이동하기 BFS로 이동

---
+++ sain/Programmers/DFS와 BFS/동빈나-BFS-블록 이동하기/main.js
@@ -0,0 +1,17 @@
+console.log("main.js");
+
+function solution(board) {
+  let answer = 0;
+
+  return answer;
+}
+
+const input = [
+  [0, 0, 0, 1, 1],
+  [0, 0, 0, 1, 0],
+  [0, 1, 0, 1, 1],
+  [1, 1, 0, 0, 1],
+  [0, 0, 0, 0, 0],
+];
+
+console.log(solution(input));
