3a4b8bb5c4b144032960957a4787d2b185a50db1
Sain Choi <chyh1900@gmail.com>
2021-05-20 14:50:16 +0900

    인프런 / 그래프와 탐색 / 경로 탐색

---
+++ sain/Inflearn/section9/9-1/main.js
@@ -0,0 +1,50 @@
+console.log("main.js");
+
+function solution(v, edges) {
+  let answer = 0;
+  const edgesCount = edges.length;
+  const visited = new Array(v).fill(false);
+  const q = [];
+
+  function dfs(l) {
+    visited[l - 1] = true;
+    q.push(l);
+    if (l >= v) {
+      console.log(q);
+      answer++;
+      return;
+    }
+
+    for (let i = 0; i < edgesCount; i++) {
+      const [from, to] = edges[i];
+      if (!visited[to - 1] && l === from) {
+        dfs(to);
+        visited[to - 1] = false;
+        q.pop();
+      }
+    }
+  }
+
+  dfs(1);
+
+  return answer;
+}
+
+const input = {
+  v: 5,
+  edges: [
+    [1, 2],
+    [1, 3],
+    [1, 4],
+    [2, 1],
+    [2, 3],
+    [2, 5],
+    [3, 4],
+    [4, 2],
+    [4, 5],
+  ],
+};
+
+const { v, edges } = input;
+
+console.log(solution(v, edges));
