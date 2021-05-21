313d73da00291a39846ae595c2cb870e2f4426fd
Sain Choi <chyh1900@gmail.com>
2021-05-21 15:57:38 +0900

    인프런 / 그래프와 탐색 / 이진트리 넓이우선탐색

---
+++ sain/Inflearn/section9/9-4/main.js
@@ -0,0 +1,25 @@
+console.log("main.js");
+
+function solution(num) {
+  let answer = "";
+  const q = [1];
+  const visited = Array(num).fill(false);
+
+  function bfs(num) {
+    while (q.length) {
+      const current = q.shift();
+      if (current > num) break;
+      answer += current + " ";
+      q.push(current * 2);
+      q.push(current * 2 + 1);
+    }
+  }
+
+  bfs(7);
+
+  return answer;
+}
+
+const input = 7;
+
+console.log(solution(input));
