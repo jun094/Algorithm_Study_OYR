2d256c59cfb597f8ad67e4e73ee23695d6fbe65e
Sain Choi <chyh1900@gmail.com>
2021-05-31 16:20:05 +0900

    a~

--- sain/Programmers/이진탐색 및 구현/동빈나-구현-문자열 압축/main.js
+++ sain/Programmers/이진탐색 및 구현/동빈나-구현-문자열 압축/main.js
@@ -5,7 +5,32 @@ function solution(input) {
   let answer = size;
   let unit = 1;
 
-  while (unit < size) {}
+  while (unit < size) {
+    let str = input;
+    let unitCnt = 1;
+    let lastUnitStr = "";
+    let resultStr = "";
+    while (str.length) {
+      const unitStr = str.substr(0, unit);
+      if (lastUnitStr.length) {
+        if (unitStr === lastUnitStr) {
+          unitCnt++;
+        } else {
+          resultStr += unitCnt > 1 ? `${unitCnt}${lastUnitStr}` : lastUnitStr;
+          unitCnt = 1;
+        }
+      }
+      lastUnitStr = unitStr;
+      str = str.substring(unit, str.length);
+      if (!str.length)
+        resultStr += unitCnt > 1 ? `${unitCnt}${lastUnitStr}` : lastUnitStr;
+    }
+    const resLength = resultStr.length;
+    if (answer > resLength) {
+      answer = resLength;
+    }
+    unit++;
+  }
 
   return answer;
 }
@@ -16,4 +41,4 @@ const input3 = "abcabcdede";
 const input4 = "abcabcabcabcdededededede";
 const input5 = "xababcdcdababcdcd";
 
-console.log(solution(input));
+console.log(solution(input1));
---
+++ sain/Programmers/이진탐색 및 구현/동빈나-구현-자물쇠와 열쇠/main.js
@@ -0,0 +1,116 @@
+console.log("main.js");
+
+function solution(key, lock) {
+  let answer = false;
+  const size = key[0].length;
+
+  function rotate(arr) {
+    const temp = Array.from(Array(size), () => new Array(size).fill(0));
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        temp[i][j] = arr[size - 1 - j][i];
+      }
+    }
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        arr[i][j] = temp[i][j];
+      }
+    }
+  }
+
+  function move(arr, dir) {
+    switch (dir) {
+      case 0:
+        for (let i = 1; i < size; i++) {
+          for (let j = 0; j < size; j++) {
+            arr[i - 1][j] = arr[i][j];
+            arr[i][j] = 0;
+          }
+        }
+        break;
+      case 1:
+        for (let i = size - 1; i > 0; i--) {
+          for (let j = 0; j < size; j++) {
+            arr[i][j] = arr[i - 1][j];
+            arr[i - 1][j] = 0;
+          }
+        }
+        break;
+      case 2:
+        for (let j = 1; j < size; j++) {
+          for (let i = 0; i < size; i++) {
+            arr[i][j - 1] = arr[i][j];
+            arr[i][j] = 0;
+          }
+        }
+        break;
+      case 3:
+        for (let j = size - 1; j > 0; j--) {
+          for (let i = 0; i < size; i++) {
+            arr[i][j] = arr[i][j - 1];
+            arr[i][j - 1] = 0;
+          }
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  function check(comp, lock) {
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        if (comp[i][j] === lock[i][j]) return false;
+      }
+    }
+    return true;
+  }
+
+  for (let i = 0; i < 3; i++) {
+    if (answer) break;
+    for (let j = 0; j < 4; j++) {
+      if (answer) break;
+      const cpKey = key.map((inner) => inner.slice());
+      while (
+        cpKey.reduce(
+          (acc, inArr) => acc + inArr.reduce((acc2, val) => acc2 + val, 0),
+          0
+        )
+      ) {
+        if (check(cpKey, lock)) {
+          answer = true;
+          break;
+        }
+        for (let i = 0; i < 4; i++) {
+          const cpcpKey = cpKey.map((inner) => inner.slice());
+          move(cpcpKey, i);
+          if (check(cpcpKey, lock)) {
+            answer = true;
+            break;
+          }
+        }
+        move(cpKey, j);
+      }
+    }
+    rotate(key);
+  }
+
+  return answer;
+}
+
+const input = {
+  key: [
+    [0, 0, 0],
+    [1, 0, 0],
+    [0, 1, 1],
+  ],
+  lock: [
+    [1, 1, 1],
+    [1, 1, 0],
+    [1, 0, 1],
+  ],
+};
+
+const { key, lock } = input;
+
+console.log(solution(key, lock));
