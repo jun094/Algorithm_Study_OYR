49806a46b51346f96d445771408d9c6926be0aa8
Sain Choi <chyh1900@gmail.com>
2021-06-18 15:47:18 +0900

    ㅇㅅㅇa

---
+++ sain/Programmers/동적계획법/정수 삼각형/main.js
@@ -0,0 +1,23 @@
+console.log("main.js");
+
+function solution(input) {
+  const n = input.length;
+  for (let i = 1; i < n; i++) {
+    const hLen = input[i].length;
+    for (let j = 0; j < hLen; j++) {
+      if (j === 0) {
+        input[i][j] += input[i - 1][0];
+      } else if (j === hLen - 1) {
+        input[i][j] += input[i - 1][hLen - 2];
+      } else {
+        input[i][j] += Math.max(input[i - 1][j - 1], input[i - 1][j]);
+      }
+    }
+  }
+
+  return Math.max(...input[n - 1]);
+}
+
+const input = [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]];
+
+console.log(solution(input));
