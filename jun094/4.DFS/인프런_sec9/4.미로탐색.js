7b7e508c4ab15a6c8acf26a7a1abff1d514818a6
choi jun young <jun094@naver.com>
2021-07-03 00:32:21 +0900

    wee10 / bfs 복습

--- jun094/4.DFS/인프런_sec9/4.미로탐색.js
+++ jun094/4.DFS/인프런_sec9/4.미로탐색.js
@@ -1,41 +1,47 @@
 const solution = (arr) => {
     let answer = 0;
     const n = arr.length;
-    let graph = arr.slice();
-    let path = [];
-
-    //방향 백터 설정
     let dx = [-1, 0, 1, 0];
     let dy = [0, 1, 0, -1];
+    let route = []; // 경로를 담을 배열
+    let tmp = [];
 
     const dfs = (x, y) => {
+        //도착점 도착
         if (x === n - 1 && y === n - 1) {
-            console.log(JSON.stringify(path));
+            route.push(tmp.slice());
             answer++;
         } else {
+            //4가지 방향 탐색
             for (let i = 0; i < 4; i++) {
                 let nx = x + dx[i];
                 let ny = y + dy[i];
 
-                if (nx >= 0 && nx <= n - 1 && ny >= 0 && ny <= n - 1 && graph[nx][ny] === 0) {
-                    graph[nx][ny] = 1; //방문 처리
-                    path.push([nx, ny]);
-                    //console.log(path);
-                    dfs(nx, ny);
+                //padding 영역이 아닐때만 탐색
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                    //벽(1)이 아닐때만 탐색
+                    if (arr[nx][ny] === 0) {
+                        //경로 담음
+                        tmp.push([nx, ny]);
 
-                    //중요 !!
-                    //뒤로 back할때는 미방문으로 바꿔줌
-                    path.pop();
-                    graph[nx][ny] = 0;
+                        //방문처리
+                        arr[nx][ny] = 1;
+
+                        //다음 방문
+                        dfs(nx, ny);
+
+                        //back !
+                        arr[nx][ny] = 0;
+                        tmp.pop();
+                    }
                 }
             }
         }
     };
-
-    path.push([0, 0]);
-    graph[0][0] = 1; // 출발지는 미리 방문
+    arr[0][0] = 1;
     dfs(0, 0);
 
+    console.log(route);
     return answer;
 };
 
